e18aaa337305cdf2439be2389e6458bf137c30dd
Rushi <rushin.mailme@gmail.com>
2024-12-14 23:40:29 +0530

    Create Blog App

---
+++ Bloggie.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bloggie.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
